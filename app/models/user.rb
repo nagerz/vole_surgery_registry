5c4a7c1166bd63ae092966cc668c4faa57d237b2
nagerz <znager@gmail.com>
2019-06-13 16:06:35 -0600

    organizes user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  has_secure_password
-
-  enum role: [:researcher, :admin]
-
   has_many :experiments
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates_presence_of :password, require: true
   validates_presence_of :location, :state
+
+  enum role: [:researcher, :admin]
+
+  has_secure_password
 end
