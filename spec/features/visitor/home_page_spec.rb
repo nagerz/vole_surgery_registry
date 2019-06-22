ecb5ab9fa9a0dac4f6af838627b9b5402d83e425
nagerz <znager@gmail.com>
2019-06-21 18:29:38 -0600

    Adds link tests to visitor home page spec

--- spec/features/visitor/home_page_spec.rb
+++ spec/features/visitor/home_page_spec.rb
@@ -18,12 +18,46 @@ RSpec.describe 'Visitor home page', type: :feature do
       end
     end
 
-    it 'I see a navigation bar' do
+    it 'I see log in and register links' do
       visit root_path
 
       within '.login' do
         expect(page).to have_button('Log In')
         expect(page).to have_link('Register')
+      end
+    end
+
+    describe 'within the navbar' do
+      describe 'I can click on' do
+        it 'the Log In link' do
+          visit root_path
+
+          within '.navbar' do
+            click_link 'Log In'
+
+            expect(current_path).to eq(login_path)
+          end
+        end
+
+        it 'the Register link' do
+          visit root_path
+
+          within '.navbar' do
+            click_link 'Register'
+
+            expect(current_path).to eq(register_path)
+          end
+        end
+
+        it 'the About link' do
+          visit root_path
+
+          within '.navbar' do
+            click_link 'About'
+
+            expect(current_path).to eq(about_path)
+          end
+        end
       end
     end
   end
